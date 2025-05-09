b5923cad9d4ec720a085233cadee7ca34ccd517e
Ellis-Ayikwei <126599954+Ellis-Ayikwei@users.noreply.github.com>
2025-05-09 14:34:48 +0000

    fix: Add full width to Homepage container for improved layout

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -208,7 +208,7 @@ const Homepage: React.FC = () => {
     ];
 
     return (
-        <div className="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100">
+        <div className="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100 w-full ">
         
 
             {/* Benefits */}
